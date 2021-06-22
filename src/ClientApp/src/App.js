1adbcdb8094a63fe91f88ebeafc1909eeb0c2341
Olo <olo@apck.de>
2021-06-22 15:24:52 +0200

    Add cards to Home.js

--- src/ClientApp/src/App.js
+++ src/ClientApp/src/App.js
@@ -17,8 +17,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
       </Layout>
     );
