d5c936812c0447b6f234f55db7f517f171043ec5
Olo <olo@apck.de>
2021-06-22 18:43:27 +0200

    Add Leaflet maps to Mission

--- src/ClientApp/src/components/Mission.js
+++ src/ClientApp/src/components/Mission.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const position = [51.505, -0.09];
 
 export class Mission extends Component {
     static displayName = Mission.name
 
     render() {
         return (
-            <div>Einsatz</div>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}
+            style={{
+                height: "800px"
+            }} >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
         );
     }
 }
